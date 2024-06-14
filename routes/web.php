77f72d605fb838d49aafa5cc6ac53d903cf10889
safiralydr <wafiqnurf@gmail.com>
2024-06-14 17:02:11 +0700

    Base Template

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
-
+use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AppController::class, 'index'])->name('app.index');
 
 Auth::routes();
 
