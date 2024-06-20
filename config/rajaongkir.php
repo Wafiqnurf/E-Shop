553c34ea64ff0580f4036416d8b7d0e35a227bb1
safiralydr <wafiqnurf@gmail.com>
2024-06-20 13:00:48 +0700

    Fix

--- config/rajaongkir.php
+++ config/rajaongkir.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'api_key' => env('RAJAONGKIR_API_KEY', null),
-    'base_url' => env('RAJAONGKIR_BASE_URL', null),
-    'origin' => env('RAJAONGKIR_ORIGIN', null),
+    'api_key' => env('RAJAONGKIR_API_KEY', 'bbeb47f711766781026cf805ba2806f1'),
+    'base_url' => env('RAJAONGKIR_BASE_URL', 'https://api.rajaongkir.com/starter/'),
+    'origin' => env('RAJAONGKIR_ORIGIN', "501"),
 ];
